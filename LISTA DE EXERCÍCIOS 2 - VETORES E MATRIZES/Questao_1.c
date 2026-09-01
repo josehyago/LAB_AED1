8a468c8ef5dfa3efde8aaab10592300ebdd8d8f1
josehyago <jose.sousa43318@alunos.ufersa.edu.br>
2026-09-01 19:50:24 -0300

    Questão 1

---
+++ LISTA DE EXERCÍCIOS 2 - VETORES E MATRIZES/Questao_1.c
@@ -0,0 +1,30 @@
+// Escreva um programa em C que solicite ao usuário o tamanho de um vetor e, em seguida, aloque dinamicamente memória para armazenar esse vetor. Depois disso, peça ao usuário que insira os elementos do vetor e, por fim, imprima os elementos na ordem inversa.
+
+#include <stdio.h>
+#include <stdlib.h> // malloc , calloc, free, realloc
+
+int main(void){
+
+    int n;
+    scanf("%d", &n);
+
+    //int vetor[n];
+    int * vetor = (int *) malloc(n*sizeof(int));
+    if(vetor == NULL){
+        printf("Nao ha memoria\n");
+        exit(1);
+    }
+
+    printf("Digite os valores: \n");
+    int index;
+    for(index = 0; index < n; index++){
+        scanf("%d", &vetor[index]);
+    }
+
+    for(index = n - 1; index >= 0; index--){
+        printf("%d ", vetor[index]);
+    }
+
+    return 0;
+}
+
